4a5c04469da0b9dfdb7d4de8d21318218101a690
Tea Kulo <teakul02@gmail.com>
2024-02-21 18:32:37 +0100

    Initial commit

---
+++ src/main/java/com/example/ethnocraftbackend/EthnocraftBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.ethnocraftbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EthnocraftBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EthnocraftBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ethnocraftbackend/EthnocraftBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ethnocraftbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EthnocraftBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
